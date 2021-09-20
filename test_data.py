38969a2b9d170ad65ebce30a9ab8aae40c27faea
Cori_Gibert_Uberto <77797840+CoriAriasGibert@users.noreply.github.com>
2021-09-21 01:28:28 +0200

    Update test_data.py

--- test_data.py
+++ test_data.py
@@ -1,11 +1,9 @@
 import pytest
-from flask import Flask, render_template, request
-
 
 from main import app, url
 
-
 #we activate the test in the shell by writing pytest
+
 
 @pytest.fixture
 def client():
@@ -19,42 +17,38 @@ def test_home(client):
     assert b'Home page' in response.data
 
 
-
 # if the address is valid
 def test_valid_url(client):
-    response = client.post('/url', data={'url': "https://lords-mobile.fr.aptoide.com/app"})
+    response = client.post(
+        '/url', data={'url': "https://lords-mobile.fr.aptoide.com/app"})
     assert b"<p><h3> App&#39;s Version  : </h3>  2.61</p>" in response.data
 
-    
 
-
-#if it is an external url
+#if it is a misspelled url
 def test_invalid_url(client):
-  with pytest.raises (Exception):
-    raise Exception(response = client.post('/url', data={'url': "https://lordsobilptoide.com/app"}))
-
-
+    response = client.post('/url', data={'url': "http://tutia.com"})
+    assert b'<h1> Misspelled address or is not an aptoide app </h1>' in response.data
 
 
 #if the url corresponds to an external link
 
+
 def test_external_url(client):
-  with pytest.raises(Exception):
-    raise Exception(response = client.post('/url', data={'url': "https://www.facebook.com/"}))
-  assert b'misspelled_address'
-
-
+    response = client.post('/url', data={'url': "https://www.facebook.com/"})
+    assert b'<h1> Misspelled address or is not an aptoide app </h1>' in response.data
 
 
 #if it is not a url
 
+
 def test_not_url(client):
-  response = client.post('/url', data={'url': "hello "})
-  assert b'error' in response.data
+    response = client.post('/url', data={'url': "hello "})
+    assert b'<title>error</title>' in response.data
+
 
 #if the url doesn't exist
 
-def test_empti_url(client):
-  response = client.post('/url', data={'url': " "})
-  assert b'error' in response.data
 
+def test_empti_url(client):
+    response = client.post('/url', data={'url': " "})
+    assert b'<title>error</title>' in response.data
